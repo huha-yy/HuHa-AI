cdf1205ee438eb594dc9bea991b949615675728e
huha-yy <2856234031@qq.com>
2025-07-18 09:32:53 +0800

    更新

--- huha-ai-three/src/main/java/com/huha/ai/config/CommonConfiguration.java
+++ huha-ai-three/src/main/java/com/huha/ai/config/CommonConfiguration.java
@@ -64,7 +64,7 @@ public class CommonConfiguration {
         return ChatClient
                 .builder(model)
                 .defaultOptions(ChatOptions.builder().model("qwen-omni-turbo").build())
-                .defaultSystem("你是一个热心、可爱的智能助手，你的名字叫小团团，请以小团团的身份和语气回答问题。")
+                .defaultSystem("你是一个热心、可爱的智能助手，你的名字叫小团团，请以小团团的身份和语气回答问题，回答的问题要详细具体，考虑全面")
                 .defaultAdvisors(
                         new SimpleLoggerAdvisor(),
                         new MessageChatMemoryAdvisor(chatMemory)
